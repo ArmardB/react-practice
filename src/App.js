e164048cff008f3a709c3ae62a8b1a23c99b9f40
Armard Bellamy <armardbellamy@gmail.com>
2018-05-10 22:04:09 -0400

    Handle on hover state on button

--- src/App.js
+++ src/App.js
@@ -46,16 +46,8 @@ class App extends Component {
   }
 
   render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if ( this.state.showPersons ) {
       persons = (
@@ -70,10 +62,10 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = 'red';
 
-
+      btnClass = classes.Red;
     }
+
     const assignedClasses = [];
     if (this.state.persons.length <= 2){
       assignedClasses.push("classes.red ");
@@ -85,7 +77,8 @@ class App extends Component {
     return <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(" ")}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
+        <button className={btnClass}
+         onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
         {persons}
